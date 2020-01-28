8b710ca44f8d56aa29370547235ff8666b7c6e7a
Stefano <stefanoferraridg@outlook.com>
2020-01-28 18:54:44 -0300

    Updates

---
+++ precache-manifest.31038b464fb7805954089862963f5708.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003554c78c2b51aa98600e8a394db567",
+    "url": "/andproducciones/index.html"
+  },
+  {
+    "revision": "0845d5618dc955a74185",
+    "url": "/andproducciones/static/css/main.60ba4de3.chunk.css"
+  },
+  {
+    "revision": "81178b85f1696712a1ed",
+    "url": "/andproducciones/static/js/2.c0639527.chunk.js"
+  },
+  {
+    "revision": "3160bf7c029258aba92ee24744af9381",
+    "url": "/andproducciones/static/js/2.c0639527.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0845d5618dc955a74185",
+    "url": "/andproducciones/static/js/main.e5a6c139.chunk.js"
+  },
+  {
+    "revision": "3a8c43f9108eb1910c0c",
+    "url": "/andproducciones/static/js/runtime-main.f4063e88.js"
+  },
+  {
+    "revision": "5e60d723f0e549ac34560ca7a4dd4cf1",
+    "url": "/andproducciones/static/media/logo.5e60d723.webp"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/and-producciones/precache-manifest.c915c09b90e45fed27aba59341e49649.js"
+  "/andproducciones/precache-manifest.31038b464fb7805954089862963f5708.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/and-producciones/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andproducciones/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
